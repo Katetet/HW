b3d8095730a574a530d940894011850e98a78514
katetet <gladkaya.kate@gmail.com>
2022-03-30 20:54:39 +0300

    HW4/1\3

--- src/HW_4_1.java
+++ src/HW_4_1.java
@@ -3,27 +3,39 @@ import java.util.Arrays;
 public class HW_4_1 {
     public static void main(String[] args) {
 
+
         System.out.println();
         System.out.println("Task 1");
-        int[] array = {9, 2, 6, 4, 5, 12, 7, 8, 6};
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(i);
-        }
         System.out.println();
-
+        int[] array = {9, 2, 6, 4, 5, 12, 7, 8, 6};
         int a;
         a = 0;
         for (int i = 0; i < array.length; i++) {
             a = a + array[i];
         }
         System.out.println(a);
+
         System.out.println();
         System.out.println("Task 2");
+        int b = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (b < array[i])
+                b = array[i];
+        }
+        System.out.println(b);
+
+        System.out.println();
+        System.out.println("Task 3");
+
+        b = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (b > array[i])
+                b = array[i];
+        }
+        System.out.println(b);
 
 
 
 
     }
-
-
 }
